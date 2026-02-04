0be451ef8700dc08c72f46f8df6fe5331a6e586f
Alona Kuznietsova <acvetochka@gmail.com>
2026-02-04 16:25:32 +0100

    add Task-class

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -16,18 +16,47 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] tasks = new string[3];
+        //string[] tasks = new string[3];
+        List<Task> tasksList = new List<Task>();
         
         public MainWindow()
         {
             InitializeComponent();
-            tasks[0] = "Домашня із С#";
-            tasks[1] = "Урок з Linux";
-            tasks[2] = "Німецька в Bussuu";
+            //tasks[0] = "Домашня із С#";
+            //tasks[1] = "Урок з Linux";
+            //tasks[2] = "Німецька в Bussuu";
 
-            ToDoListBox.ItemsSource = tasks;
+            Task task1 = new Task();
+            task1.Name = "Домашня із С#";
+            task1.Description = "Додайте до класу Task, у ToDoApplication проекті, властивість Description яка буде зберігати більш детальний опис задачі. Зробіть щоб при подвійному кліку на задачу в списку виводилося значення властивості Description у MessageBox.";
+            task1.isCompleted = false;
+
+            Task task2 = new Task();
+            task2.Name = "Урок з Linux";
+            task2.Description = "Розділ Операційна система";
+            task2.isCompleted = false;
+
+            Task task3 = new Task();
+            task3.Name = "Німецька в Bussuu";
+            task3.Description = "Пройти декілька уроків";
+            task3.isCompleted = false;
+
+            tasksList.Add(task1);
+            tasksList.Add(task2);
+            tasksList.Add(task3);
+
+            ToDoListBox.ItemsSource = tasksList;
+            ToDoListBox.DisplayMemberPath = "Name";
 
         }
 
+        private void ToDoListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Task selected = ToDoListBox.SelectedItem as Task;
+            if (selected != null)
+            {
+                MessageBox.Show(selected.Description);
+            }
+        }
     }
 }
---
+++ ToDoApp/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    internal class Task
+    {
+        public string Name { get; set; }
+
+        public bool isCompleted  { get; set; }
+
+        public string Description { get; set; }
+    }
+}
